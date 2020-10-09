6c65a5328dd15cae1dc0817bf6311fae97a53f33
Zahra <zahradeenie@gmail.com>
2020-10-09 21:22:05 +0200

    feat: add function clause to match a response with an id

--- lib/server/handler.ex
+++ lib/server/handler.ex
@@ -1,8 +1,8 @@
 defmodule Server.Handler do
   def handle(request) do
-    request 
+    request
     |> parse
-    |> IO.inspect
+    |> IO.inspect()
     |> route
     |> format_response
   end
@@ -11,10 +11,15 @@ defmodule Server.Handler do
     [method, path, _] =
       request
       |> String.split("\n")
-      |> List.first
+      |> List.first()
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
   def route(conv) do
@@ -22,16 +27,28 @@ defmodule Server.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{conv | resp_body: "Bears, Lions, Tigers", status: 200}
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{conv | resp_body: "Teddy, Smokey, Paddington", status: 200}
+  end
+
+  def route(conv, "GET", "/bears" <> id) do
+    %{conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(conv, "DELETE", "/bears" <> id) do
+    %{conv | resp_body: "Deleting Bear #{id} is forbidden", status: 403}
+  end
+
+  def route(conv, _, path) do
+    %{conv | resp_body: "No #{path} here", status: 404}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -39,6 +56,16 @@ defmodule Server.Handler do
     """
   end
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error",
+    }[code]
+  end
 end
 
 request = """
@@ -48,11 +75,32 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
+
 response = Server.Handler.handle(request)
 IO.puts(response)
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Server.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Server.Handler.handle(request)
+IO.puts(response)
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
